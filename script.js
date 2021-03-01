07321bcd4af6148913dc890311c31aa26e03a26c
alchoz <djswjat@gmail.com>
2021-03-01 16:32:48 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,27 @@
 function compute() {
-    p = document.getElementById("principal").value;
-
+  
     var principal = document.getElementById("principal").value;
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
 
-    interest = principal * years * rate / 100;
-
-    var currentTime = new Date(); //gets current year
-    currentDate.setFullYear(currentDate.getFullYear() + parseInt(years));
-    var displayYear = currentDate.getFullYear();
-
+   
+    var interest = principal * years * rate / 100;
+    var endYear = new Date().getFullYear() + parseInt(years);
     
-    document.getElementById("result").innerHTML = 'If you deposit ' + "<mark>"+ principal + '$ ' +"</mark>" + "<br/>" +
-        'at an interest rate of ' + "<mark>"+ rate + '%. ' + "</mark>" + "<br/>" +
-        'you will recieve ' + "<mark>" + interest + '$ ' + "</mark>" + "<br/>" +
-        'in the year of ' + "<mark>" + displayYear + "</mark>";
-    
+    document.getElementById("result").innerHTML = `If you deposit <mark>${principal}</mark>,
+    <br>at an interest rate of <mark>${rate}</mark>.
+    <br>You will receive an amount of <mark>${interest.toFixed(2)}</mark>,
+    <br>in the year <mark>${endYear}</mark>`;
+
     if (principal <= 0) {
         alert("Enter a positive number");
         document.getElementById("principal").focus();
         return false;
     }
-
 }
 
-function readValue() {
 
-    var getRate = document.getElementById("rate").value;  //gets an elemnt and stores it in the variable
-    var showRate = getRate;
-    document.getElementById("show_rate").innerHTML = showRate + '%';
-
+function rangeReader() {
+    var sliderValue = document.getElementById("rate").value;
+    document.getElementById("sliderValue").innerText = `${sliderValue}%`;
 }
-
