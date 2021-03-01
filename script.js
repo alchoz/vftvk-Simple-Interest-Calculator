0328805f59722e4b7068cd28bec3b6ce3b35a28d
alchoz <djswjat@gmail.com>
2021-03-01 16:03:45 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function compute() {
     p = document.getElementById("principal").value;
 
-    var principal = +document.getElementById("principal").value;
-    var rate = +document.getElementById("rate").value;
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
 
     interest = principal * years * rate / 100;
@@ -11,17 +11,17 @@ function compute() {
     currentDate.setFullYear(currentDate.getFullYear() + parseInt(years));
     var displayYear = currentDate.getFullYear();
 
+    
+    document.getElementById("result").innerHTML = 'If you deposit ' + "<mark>"+ principal + '$ ' +"</mark>" + "<br/>" +
+        'at an interest rate of ' + "<mark>"+ rate + '%. ' + "</mark>" + "<br/>" +
+        'you will recieve ' + "<mark>" + interest + '$ ' + "</mark>" + "<br/>" +
+        'in the year of ' + "<mark>" + displayYear + "</mark>";
+    
     if (principal <= 0) {
         alert("Enter a positive number");
         document.getElementById("principal").focus();
         return false;
     }
-
-
-    document.getElementById("result").innerHTML = 'If you deposit ' + principal + '$ ' + "<br/>" +
-        'at an interest rate of ' + rate + '%. ' + "<br/>" +
-        'you will recieve ' + interest + '$ ' + "<br/>" +
-        'in the year of ' + displayYear;
 
 }
 
